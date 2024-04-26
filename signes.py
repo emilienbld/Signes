16602b898be5bf15c4db43d32c1be0f49d65ca64
emilienbld <98468414+emilienbld@users.noreply.github.com>
2024-04-26 15:38:19 +0200

    Update signes.py

--- signes.py
+++ signes.py
@@ -18,7 +18,7 @@ def control_cursor(hand_landmarks):
     x, y = int((1 - x) * screen_width), int(y * screen_height)  # Notez l'inversion de l'axe des x ici
     # Déplacer le curseur de la souris
     pyautogui.moveTo(x, y)
-    # # Récupérer les coordonnées des extrémités du pouce et de l'index
+    # Récupérer les coordonnées des extrémités du pouce et de l'index
     thumb_tip = hand_landmarks.landmark[mp.solutions.hands.HandLandmark.THUMB_TIP]
     index_finger_tip = hand_landmarks.landmark[mp.solutions.hands.HandLandmark.INDEX_FINGER_TIP]
     # Calculer la distance entre le pouce et l'index
@@ -60,7 +60,7 @@ def control_volume(hand_landmarks):
     # Récupérer les limites de volume
     min_volume_level, max_volume_level, _ = volume_control.GetVolumeRange()
     # Définir un seuil pour la distance entre le pouce et l'index
-    threshold_distance = 0.05  # Vous pouvez ajuster cette valeur en fonction de vos besoins
+    threshold_distance = 0.05
     if distance < threshold_distance:  # Si la distance est inférieure au seuil (pouce et index proches)
         new_volume_level = current_volume_level - 1.0
         if new_volume_level >= min_volume_level:
@@ -81,13 +81,11 @@ def control_browser(hand_landmarks):
     distance_middle_ring = np.sqrt((middle_finger_tip.x - ring_finger_tip.x)**2 + (middle_finger_tip.y - ring_finger_tip.y)**2 + (middle_finger_tip.z - ring_finger_tip.z)**2)
     distance_thumb_middle = np.sqrt((thumb_tip.x - middle_finger_tip.x)**2 + (thumb_tip.y - middle_finger_tip.y)**2 + (thumb_tip.z - middle_finger_tip.z)**2)
 
-    threshold_distance_close = 0.03  # Augmenter la valeur pour rendre le signe plus facile à faire
-    threshold_distance_far = 0.05  # Augmenter la valeur pour rendre le signe plus facile à faire
+    threshold_distance_close = 0.03
+    threshold_distance_far = 0.05
 
     if distance_index_pinky > threshold_distance_far and distance_middle_ring < threshold_distance_close and distance_thumb_middle < threshold_distance_close:
         webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-
-
 
 def detect_hand_landmarks(img, hands):
     output_img = img.copy()
@@ -107,7 +105,6 @@ def detect_hand_landmarks(img, hands):
             control_brightness(hand_landmarks)
             control_browser(hand_landmarks)
     return output_img
-
 
 # Initialisation de MediaPipe Hands
 landmarker = mp.solutions.hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
